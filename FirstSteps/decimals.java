d4c437c257246750b74eeea07630b809892689a7
rud-rax <rudrakshnaik8990@gmail.com>
2022-12-07 01:06:53 +0530

    Integers 3

---
+++ FirstSteps/decimals.java
@@ -0,0 +1,12 @@
+// package FirstSteps;
+
+public class decimals {
+    public static void main(String[] args) {
+
+        // FLOAT
+        float myFloatNum = 4.89F ;
+        System.out.println("The float variable is assigned to a value of " + myFloatNum);
+        System.out.println("Float Value Range is from " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        
+    }
+}
